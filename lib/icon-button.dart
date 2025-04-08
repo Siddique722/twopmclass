00bd2f023ff3f227faf0da09c14fcbe830b82071
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2025-04-08 21:50:10 +0500

    update code

---
+++ lib/icon-button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class IconButtonClass extends StatelessWidget {
+  const IconButtonClass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff53B175),
+      body: Center(),
+    );
+  }
+}
--- lib/screens/authScreen/signupScreen/signupscreen.dart
+++ lib/screens/authScreen/signupScreen/signupscreen.dart
@@ -8,50 +8,80 @@ class SignUpScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
 
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          child:
+
+
+          Column(
             children: [
-          // use for addition
-          TextButton(
-            child:Text("Add",style: TextStyle(color: Colors.purple),),
-            onPressed:(){
-              // console coding
-              // cout, console.writeLine()
-              // dart programming language-
-              print('message');
-              // variables---
-              //store the value
-              // data type:- homework
-              // int, double, char, String, bool
-              // rules for naming a variable:-
-              int n1=5;
-              int n2=9;
-              // cout<<n1
-              // operators----
-              // types of operators
-              // 1.Arithmatic operator--
-              // +, -,*, /, %
-              int add=n1+n2;
-              // cout<<"add="<<add;
-              // add=14
-              print('n1=$n1');
-              print("value of n2=$n2");
-              print(add);
-              print("add=$add");
+              // row for text buttons----
+              Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+              // use for addition
+              Container(
 
-              // ui coding
-            }
-          ),
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Colors.purpleAccent,
+                  borderRadius: BorderRadius.circular(50)
+                ),
+                child: TextButton(
+                  child:Text("Add",style: TextStyle(color: Colors.white),),
+                  onPressed:(){
+                    // console coding
+                    // cout, console.writeLine()
+                    // dart programming language-
+                    print('message');
+                    // variables---
+                    //store the value
+                    // data type:- homework
+                    // int, double, char, String, bool
+                    // rules for naming a variable:-
+                    int n1=5;
+                    int n2=9;
+                    // cout<<n1
+                    // operators----
+                    // types of operators
+                    // 1.Arithmatic operator--
+                    // +, -,*, /, %
+                    int add=n1+n2;
+                    // cout<<"add="<<add;
+                    // add=14
+                    print('n1=$n1');
+                    print("value of n2=$n2");
+                    print(add);
+                    print("add=$add");
 
-          // use for substraction
-          TextButton(onPressed: (){
-            int a=90;
-            int b=80;
-            int sub=a-b;
-            print('Substractioin=$sub');
-          }, child: Text('Substract'))
-        ],
-      ))
+                    // ui coding
+                  }
+                ),
+              ),
+
+              // use for substraction
+              TextButton(onPressed: (){
+                int a=90;
+                int b=80;
+                int sub=a-b;
+                print('Substractioin=$sub');
+              }, child: Text('Substract'))
+                      ],
+                    ),
+
+              //-----row icon buttons--------
+              Row(children: [
+                CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  child: IconButton(
+                    onPressed: (){},
+                    icon: Icon(Icons.add),
+                  ),
+                )
+              ],)
+
+
+            ],
+          )
+      )
     );
   }
 }
